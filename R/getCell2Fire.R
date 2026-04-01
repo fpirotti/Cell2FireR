d071dcca3d77dcea65f2116e36b6d9c5b329785d
fpirotti <fpirotti@gmail.com>
2026-04-01 17:43:55 +0200

    fixed update packages

--- R/getCell2Fire.R
+++ R/getCell2Fire.R
@@ -7,30 +7,45 @@
 #' @examples #none
 getCell2Fire <- function(outdir) {
   dest <- file.path(tempdir(), "Cell2FireR")
-  if (!dir.exists(dest)) {
-    utils::download.file(
-      "https://github.com/fire2a/C2F-W/releases/download/v1.0.1/Cell2FireW_v1.0.1.zip",
-      destfile = paste0(dest, ".zip")
-    )
-    utils::unzip(paste0(dest, ".zip"), exdir = dest)
+  out <- tryCatch({
+    
+    if (!dir.exists(dest)) {
+      utils::download.file(
+        "https://github.com/fire2a/C2F-W/releases/download/v1.0.1/Cell2FireW_v1.0.1.zip",
+        destfile = paste0(dest, ".zip")
+      )
+      utils::unzip(paste0(dest, ".zip"), exdir = dest)
+    }
+    
+    fp <- file.path(dest, "C2F","Cell2Fire")
+    exe.linux <- file.path(fp,"Cell2Fire")
+    exe.win <- file.path(fp, "Cell2Fire.exe")
+    
+    if(!file.exists(exe.linux) ){
+      warning("Linux compiled Cell2Fire does not exist")
+    }
+    
+    if( !file.exists(exe.win)){
+      warning("Windows compiled Cell2Fire does not exist")
+    }
+    Sys.chmod(exe.linux, mode = "0777", use_umask = TRUE)
+    Sys.chmod(exe.win, mode = "0777", use_umask = TRUE)
+    file.copy( list.files(fp, pattern="*\\.so$|^Cell2Fire$", full.names = T ) ,
+               to = outdir, overwrite = T )
+    file.copy( list.files(fp, pattern="*\\.dll$|*\\.exe$", full.names = T ) ,
+               to = outdir, overwrite = T )
+    TRUE
+  },
+  error = function(e) { message("  Error reading ground: ", e$message); return(FALSE) }
+  )
+  
+  if(out){ 
+    packageStartupMessage("Installation of the Cell2Fire C2F package,
+successful")
+  } else { 
+    packageStartupMessage("Something went wrong in installation of Cell2Fire,
+contact the developers")
   }
+  
 
-  fp <- file.path(dest, "C2F","Cell2Fire")
-  exe.linux <- file.path(fp,"Cell2Fire")
-  exe.win <- file.path(fp, "Cell2Fire.exe")
-
-  if(!file.exists(exe.linux) ){
-    warning("Linux compiled Cell2Fire does not exist")
-  }
-
-  if( !file.exists(exe.win)){
-    warning("Windows compiled Cell2Fire does not exist")
-  }
-  Sys.chmod(exe.linux, mode = "0777", use_umask = TRUE)
-  Sys.chmod(exe.win, mode = "0777", use_umask = TRUE)
-  file.copy( list.files(fp, pattern="*\\.so$|^Cell2Fire$", full.names = T ) ,
-             to = outdir, overwrite = T )
-  file.copy( list.files(fp, pattern="*\\.dll$|*\\.exe$", full.names = T ) ,
-             to = outdir, overwrite = T )
-  TRUE
 }
--- R/onload.R
+++ R/onload.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname, pkgname) {
-  if (interactive())  packageStartupMessage("Checking for Cell2Fire executable...")
+  
+   
 
   bindir <- system.file("bin", package = pkgname)
   outdir <- file.path(bindir, "C2F")
@@ -8,21 +9,11 @@
     dir.create(outdir )
   } else{
     if(file.exists(exewinfile)){
-      if (interactive())  packageStartupMessage("Cell2Fire already available.")
       return()
     }
   }
 
-  if (interactive())  packageStartupMessage("Cell2Fire NOT available, installing...")
-
   outdir <- file.path(system.file("bin", package = pkgname), "C2F")
-  packageStartupMessage(outdir)
-  if(!getCell2Fire(outdir)){
-    if (interactive()) packageStartupMessage("Something went wrong in installation of the C2F package,
-                          contact the developers")
-  } else {
-    if (interactive())  packageStartupMessage("Cell2Fire successfully installed")
-
-  }
+  getCell2Fire(outdir)  
 
 }
